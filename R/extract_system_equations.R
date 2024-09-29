0fe82db406eef0d644275e5230eaa62c075424c0
Damie Pak <dkp5177@psu.edu>
2024-09-29 17:31:30 -0400

    Finished the extract_system_equations and it is functinoing

--- R/extract_system_equations.R
+++ R/extract_system_equations.R
@@ -1,7 +1,18 @@
 
-extract_system_equations <- function(deSolve_function,
-                                    user_params,
-                                    user_times){
+#' Extract the system equations from a deSolve function
+#'
+#' By feeding in a deSolve function, you can then retrieve the state-variables
+#' of interest as well as the corresponding equations related to their derivative
+#'
+#'
+#' @param deSolve_function
+
+#' @return A list element with the first element being the state variables
+#' and the second element being a list of the equations.
+#' @export
+#'
+#' @examples
+extract_system_equations <- function(deSolve_function){
 
  if(inherits(deSolve_function,"function") == FALSE){
    stop("Ensure that you are giving me a function!")
@@ -10,37 +21,47 @@ extract_system_equations <- function(deSolve_function,
  #Take the deSolve function and make each line by line a list element
  #remove spaces and ensure that we use equal signs only
  deparsed_equations <- deparse(deSolve_function)
-
- ###
-
- #Pull out the state-variables from the equations
- state_variables <- lapply(deparsed_equations, function(x)
-                          str_extract(x, "(?<=return\\(list\\(c\\()[^\\)]+")) |>
-                          unlist(use.names = FALSE)
+ deparsed_equations <- lapply(deparsed_equations, function(x) gsub(" ", "", x))
+ deparsed_equations <- lapply(deparsed_equations, function(x) gsub(" ", "", x))
 
 
- state_variables <- as.character(state_variables[!is.na(state_variables)])
+ #Find the return equation
+ return_equation <- lapply(deparsed_equations, function(x)
+                    str_extract(x, "(?<=return\\(list\\(c\\()[^\\)]+"))
 
-
- if(is.na(state_variables) == TRUE){
-   stop("Cannot find the state-variables, check!")
+ if(length(return_equation) == 0){
+   stop("Cannot find the return() of state-variables, check!")
  }
 
- state_variables_list <- as.list(unlist(strsplit(state_variables, ",")))
+ #What line is the return equation in.
+ deparsed_return_index <-which(!is.na(unlist(return_equation)))
 
- for (state in 1:length(state_variables_list)){
-   state_var <- state_variables_list[[state]]
+ #Pull out the state-variables from the return equation
+ state_variables <- return_equation[[deparsed_return_index]]
+ state_variables_vector <- unlist(strsplit(state_variables, split = ","))
 
-   print(state_var)
+ #All equations excluding the return
+ deparsed_equations_noreturn <- deparsed_equations[-deparsed_return_index]
+
+ ### Pulls out the equations from the deSolve model
+ equations_list <- NULL
+ for (state in 1:length(state_variables_vector)){
+   state_var <- state_variables_vector[[state]] #state variable of interest
+
    ###which index matches
-   equation_index <- which(unlist(lapply(deparsed_equations, function(x)
-                              str_detect(x, state_var)) == TRUE))
+   equation_index <- which(unlist(lapply(deparsed_equations_noreturn, function(x)
+                                  str_detect(x, state_var)) == TRUE))
 
-
-   return(deparsed_equations)
+   #return the equation for the derivative of the state variable
+   equations_list[[state]] <- deparsed_equations_noreturn[[equation_index]]
 
  }
 
+ if(length(state_variables_vector) != length(equations_list)) {
+   stop("The number of equations and the number of state variables are not equal!")
+ }
 
-
+ return(list(state_variables_vector,
+             equations_list))
 }
+
